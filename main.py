962ac4f76181c28a0acf8af6911d651896e2133c
zephyr-zdz <67674877+zephyr-zdz@users.noreply.github.com>
2021-10-28 08:35:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,11 +14,12 @@ import requests
 import numpy
 from PIL import Image
 from PIL import ImageEnhance
+
 from requests import session, post, adapters
 adapters.DEFAULT_RETRIES = 5
 
 PUSH_KEY = getenv("KEY")
-print(getenv("KEY"))
+
 class Fudan:
     """
     建立与复旦服务器的会话，执行登录/登出操作
